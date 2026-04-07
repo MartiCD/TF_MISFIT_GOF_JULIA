c8004323850dc2b6177a116da21b82f0eab6099d
matiscd <marticircunsiduxans@gmail.com>
2026-04-07 10:03:43 +0200

    refactor: package root
    add exports and includes

--- src/TFMisfitGOF.jl
+++ src/TFMisfitGOF.jl
@@ -1,11 +1,20 @@
 module TFMisfitGOF
 
-export compute_from_inputfile,
-        run_legacy_script,
-        validate_example_run,
-        main 
+export KN, NF, W0,
+       morlet, cwt,
+       fcoolr!, fcoolr_complex,
+       tf_misfits_glob, tf_misfits_loc,
+       strip_quotes, parse_fortran_logical, read_fortran_namelist_input,
+       write_1d, write_2d_slices,
+       compute_from_inputfile, main
 
-include("legacy.jl")
+include("constants.jl")
+include("fft_legacy.jl")
+include("cwt.jl")
+include("misfit_global.jl")
+include("misfit_local.jl")
+include("input_parsing.jl")
+include("io_legacy.jl")
 include("api.jl")
 include("cli.jl")
 
